c81950e8bf74823b02f9a0ce719da11dbb983d65
UtkarshAhuja2003 <ahujautkarsh80@gmail.com>
2023-10-15 18:11:57 +0530

    feat: Implement batch API for retrieving multiple IP details

--- server/app.js
+++ server/app.js
@@ -8,10 +8,23 @@ const reader = Reader.openBuffer(buffer);
 
 const app = new Router()
   .get('/', () => new Response(Bun.file(__dirname + '/www/index.html')))
-  .get('/city/:ip', (ctx) => {
-    const resp = reader.city(ctx.params.ip);
-    return Response.json(resp);
-  });
+
+app.post('/city/batch', async (req) => {
+  try {
+    const { ips } = await req.json();  // Extract the 'ips' array from the request body
+
+    // Create an array of promises, each promise resolves to the city corresponding to the IP address
+    const promises = ips.map(async (ip) => {
+      return reader.city(ip);
+    });
+    // Wait for all promises to settle and collect the results
+    const results = await Promise.all(promises);
+
+    return new Response(JSON.stringify(results));
+  } catch (error) {
+    return new Response('Error processing IP addresses', { status: 500 });
+  }
+});
 
 app.use(404, () => {
   return new Response(Bun.file(__dirname + '/www/404.html'))
