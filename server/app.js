f75423aaa1b83d3d31447314ced81fef4f06fb2a
Chakshu Gautam <chaks.gautam@gmail.com>
2023-10-15 10:30:31 +0530

    test

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const app = new Router()
   .get('/home', () => new Response(Bun.file(__dirname + '/www/index.html')))
   .get('/city/:ip', (ctx) => {
     const resp = reader.city(ctx.params.ip);
-    return Response.json(JSON.stringify(resp));
+    return Response.json(resp);
   });
 
 app.use(404, () => {
