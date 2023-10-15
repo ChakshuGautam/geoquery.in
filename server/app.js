d4a0309ced1740985baa0bc8691cd76edb3a2361
UtkarshAhuja2003 <ahujautkarsh80@gmail.com>
2023-10-15 22:12:22 +0530

    chore: Update /city/batch endpoint

--- server/app.js
+++ server/app.js
@@ -8,23 +8,27 @@ const reader = Reader.openBuffer(buffer);
 
 const app = new Router()
   .get('/', () => new Response(Bun.file(__dirname + '/www/index.html')))
-
-app.post('/city/batch', async (req) => {
-  try {
-    const { ips } = await req.json();  // Extract the 'ips' array from the request body
-
-    // Create an array of promises, each promise resolves to the city corresponding to the IP address
-    const promises = ips.map(async (ip) => {
-      return reader.city(ip);
-    });
-    // Wait for all promises to settle and collect the results
-    const results = await Promise.all(promises);
-
-    return new Response(JSON.stringify(results));
-  } catch (error) {
-    return new Response('Error processing IP addresses', { status: 500 });
-  }
-});
+  .get('/city/:ip', (ctx) => {
+    const resp = reader.city(ctx.params.ip);
+    return Response.json(resp);
+  })
+  .post('/city/batch', async (req) => {
+    try {
+      const { ips } = await req.json();  // Extract the 'ips' array from the request body
+  
+      // Create an array of promises, each promise resolves to the city corresponding to the IP address
+      const promises = ips.map(async (ip) => {
+        const response = reader.city(ip);
+        return response; 
+      });
+      // Wait for all promises to settle and collect the results
+      const results = await Promise.all(promises);
+  
+      return Response.json(results, { status: 200 });
+    } catch (error) {
+      return new Response('Error processing IP addresses', { status: 500 });
+    }
+  });
 
 app.use(404, () => {
   return new Response(Bun.file(__dirname + '/www/404.html'))
