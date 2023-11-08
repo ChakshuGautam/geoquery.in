dabf02ce50934a990a8fa6d4904f319844794586
Srijan Srivastava <codeofficialsrijansriv@gmail.com>
2023-11-08 12:02:45 +0530

    Added workflow CI for test and created sample tests. (#16)
    Co-authored-by: Srijan-SS02 <Srijan-SS02@users.noreply.github.com>

---
+++ server/__tests__/sample.test.js
@@ -0,0 +1,43 @@
+import request from 'supertest';
+import app from '../app.js';
+
+describe('GET /', () => {
+  it('should return the index.html page', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.headers['content-type']).toContain('text/html');
+  });
+});
+
+describe('GET /city/:ip', () => {
+  it('should return geolocation data for a valid IP', async () => {
+    const response = await request(app).get('/city/8.8.8.8'); // Replace with a valid IP address
+    expect(response.status).toBe(200);
+    expect(response.body.status).toBe('success');
+  });
+
+  it('should return an error for an invalid IP', async () => {
+    const response = await request(app).get('/city/invalid_ip');
+    expect(response.status).toBe(200); // Handle error gracefully in your app, so it returns 200 status
+    expect(response.body.status).toBe('fail');
+  });
+});
+
+describe('POST /city/batch', () => {
+  it('should return geolocation data for an array of valid IPs', async () => {
+    const ips = ['8.8.8.8', '4.4.4.4']; // Replace with valid IP addresses
+    const response = await request(app)
+      .post('/city/batch')
+      .send({ ips });
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+
+  it('should handle errors when processing IP addresses', async () => {
+    const ips = ['8.8.8.8', 'invalid_ip']; // Include an invalid IP
+    const response = await request(app)
+      .post('/city/batch')
+      .send({ ips });
+    expect(response.status).toBe(500); // Handle error gracefully in your app, so it returns 500 status
+  });
+});
