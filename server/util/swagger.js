78fcbe2fe61f5fad25cd2f0c65964960e4b5b08e
Aryan Mishra <aryan29mishra@gmail.com>
2023-12-11 12:09:32 +0530

    Added the Open API Documentation (#17)

---
+++ server/util/swagger.js
@@ -0,0 +1,33 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+import {generateHTML, serve, setup} from 'swagger-ui-express';
+import yaml from 'js-yaml';
+import fs from 'fs';
+
+const version = process.env.npm_package_version;
+
+const definition = yaml.load(fs.readFileSync('./spec.yaml', 'utf8'));
+const schemas = yaml.load(fs.readFileSync('./schemas.yaml', 'utf8'));
+
+definition.components.schemas = schemas.components.schemas;
+const options = {
+    definition: definition,
+    apis: ['./app.js']
+
+};
+
+const specs = swaggerJsdoc(options);
+
+console.log(Response.json(serve))
+console.log(new Response(setup(specs)).formData)
+
+function swagger(app) {
+    // Swagger Page
+    app.use('/docs', serve, setup(specs));
+    
+    // app.get('/docs.json', (ctx) => {
+    //     ctx.headers.set('content-type','application/json');
+    //     return Response.json(specs);
+    // });
+}
+
+export default swagger;
