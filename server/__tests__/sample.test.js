d447747115bb9854cde3d481079f5aacf59ee80d
Dhruv Baliyan <dhruvbaliyan1001@gmail.com>
2024-01-19 10:42:24 +0530

    test: add tests for /location/centroid API

--- server/__tests__/sample.test.js
+++ server/__tests__/sample.test.js
@@ -1,65 +1,90 @@
-import request from 'supertest';
-import { app } from '../app.js';
+import { app } from '../app.js'
 
-const BASE_URL = `http://localhost:${app.port}`;
+const BASE_URL = `http://0.0.0.0:${app.port}`;
 
 describe('GET /', () => {
   it('should return the index.html page', async () => {
     const response = await fetch(BASE_URL);
     expect(response.status).toBe(200);
-    expect(response.headers['content-type']).toContain('text/html');
+    expect(response.headers.get('content-type')).toContain('text/html');
   });
 });
 
 describe('GET /city/:ip', () => {
   it('should return geolocation data for a valid IP', async () => {
-    const response = await request(app).get('/city/8.8.8.8'); // Replace with a valid IP address
+    const response = await fetch(`${BASE_URL}/city/8.8.8.8`); // Replace with a valid IP address
+    const jsonResponse = await response.json();
     expect(response.status).toBe(200);
-    expect(response.body.status).toBe('success');
+    expect(jsonResponse.status).toBe('success');
   });
 
   it('should return an error for an invalid IP', async () => {
-    const response = await request(app).get('/city/invalid_ip');
+    const response = await fetch(`${BASE_URL}/city/invalid_ip`);
+    const jsonResponse = await response.json();
     expect(response.status).toBe(200); // Handle error gracefully in your app, so it returns 200 status
-    expect(response.body.status).toBe('fail');
+    expect(jsonResponse.status).toBe('fail');
   });
 });
 
 describe('POST /city/batch', () => {
   it('should return geolocation data for an array of valid IPs', async () => {
-    const ips = ['8.8.8.8', '4.4.4.4']; // Replace with valid IP addresses
-    const response = await request(app)
-      .post('/city/batch')
-      .send({ ips });
+    const response = await fetch(`http://0.0.0.0:3000/city/batch`, {
+      method: 'POST',
+      body: JSON.stringify(['8.8.8.8', '4.4.4.4']),  // Replace with valid IP addresses
+      headers: { "Content-Type": "application/json" },
+    });
+    const jsonResponse = await response.json();
     expect(response.status).toBe(200);
-    expect(Array.isArray(response.body)).toBe(true);
+    expect(Array.isArray(jsonResponse)).toBe(true);
   });
 
   it('should handle errors when processing IP addresses', async () => {
-    const ips = ['8.8.8.8', 'invalid_ip']; // Include an invalid IP
-    const response = await request(app)
-      .post('/city/batch')
-      .send({ ips });
-    expect(response.status).toBe(500); // Handle error gracefully in your app, so it returns 500 status
+    const response = await fetch(`${BASE_URL}/city/batch`, {
+      method: 'POST',
+      body: JSON.stringify(['8.8.8.8', 'invalid_id']),  // Replace with valid IP addresses
+      headers: { "Content-Type": "application/json" },
+    });
+    const jsonResponse = await response.json();
+    expect(response.status).toBe(200); // Handle error gracefully in your app, so it returns 500 status
+    expect(jsonResponse[1].status).toBe('fail');
   });
 });
 
 describe('GET /georev', () => {
-  // Test with valid latitude and longitude
   it('returns location data for valid coordinates', async () => {
     const latitude = '26.8756';  // Use a valid latitude
     const longitude = '80.9115'; // Use a valid longitude
-    const response = await request(app).get(`/georev?lat=${latitude}&lon=${longitude}`);
+    const response = await fetch(`/georev?lat=${latitude}&lon=${longitude}`);
+    const jsonResponse = await response.json();
     expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('status', 'success');
+    expect(jsonResponse).toHaveProperty('status', 'success');
   });
 
-  // Test with invalid latitude and longitude
   it('returns an error for invalid coordinates', async () => {
     const latitude = 'invalid_lat';  // Invalid latitude
     const longitude = 'invalid_lon'; // Invalid longitude
-    const response = await request(app).get(`/georev?lat=${latitude}&lon=${longitude}`);
+    const response = await fetch(`/georev?lat=${latitude}&lon=${longitude}`);
+    const jsonResponse = await response.json();
     expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('status', 'fail');
+    expect(jsonResponse).toHaveProperty('status', 'fail');
   });
+});
+
+describe('POST /location/centroid', () => {
+  it ('should return lat lon for given district name in query', async() => {
+    const response = await fetch(`${BASE_URL}/location/centroid?district=lucknow`);
+    const jsonResponse = await response.json();
+    expect(response.status).toBe(200);
+    expect(jsonResponse).toEqual({
+      district: "Lucknow",
+      state: "UTTAR PRADESH",
+      lon: 80.89119983155268,
+      lat: 26.830190863213858,
+    });
+  })
+
+  it ('should return error for invalid district name in query', async() => {
+    const response = await fetch(`${BASE_URL}/location/centroid?district=lalaland`);
+    expect(response.status).toBe(404);
+  })
 });
