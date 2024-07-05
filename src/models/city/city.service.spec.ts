3f49db739e79d5347fda29ee6602eee320d17799
35C4n0r <jaykumar20march@gmail.com>
2024-07-05 12:33:59 +0530

    fix: fix city.service.spec.ts test.
    Signed-off-by: 35C4n0r <jaykumar20march@gmail.com>

--- src/models/city/city.service.spec.ts
+++ src/models/city/city.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CityService } from './city.service';
+import { Logger } from '@nestjs/common';
 
 describe('CityService', () => {
   let service: CityService;
@@ -26,52 +27,53 @@ describe('CityService', () => {
   });
 
   it('should return city information when IP provided', () => {
-    const ip = '2401:4900:1c82:377e:bb89:dfc9:28a1:1e06';
+    const ip = '8.8.8.8';
     const mockCityInfo = {
       continent: {
-        code: 'AS',
-        geonameId: 6255147,
+        code: 'NA',
+        geonameId: 6255149,
         names: {
-          de: 'Asien',
-          en: 'Asia',
-          es: 'Asia',
-          fr: 'Asie',
-          ja: 'アジア',
-          'pt-BR': 'Ásia',
-          ru: 'Азия',
-          'zh-CN': '亚洲',
+          de: 'Nordamerika',
+          en: 'North America',
+          es: 'Norteamérica',
+          fr: 'Amérique du Nord',
+          ja: '北アメリカ',
+          'pt-BR': 'América do Norte',
+          ru: 'Северная Америка',
+          'zh-CN': '北美洲',
         },
       },
       country: {
-        geonameId: 1269750,
-        isoCode: 'IN',
+        geonameId: 6252001,
+        isoCode: 'US',
         names: {
-          de: 'Indien',
-          en: 'India',
-          es: 'India',
-          fr: 'Inde',
-          ja: 'インド',
-          'pt-BR': 'Índia',
-          ru: 'Индия',
-          'zh-CN': '印度',
+          de: 'USA',
+          en: 'United States',
+          es: 'Estados Unidos',
+          fr: 'États Unis',
+          ja: 'アメリカ',
+          'pt-BR': 'EUA',
+          ru: 'США',
+          'zh-CN': '美国',
         },
       },
       registeredCountry: {
-        geonameId: 1269750,
-        isoCode: 'IN',
+        geonameId: 6252001,
+        isoCode: 'US',
         names: {
-          de: 'Indien',
-          en: 'India',
-          es: 'India',
-          fr: 'Inde',
-          ja: 'インド',
-          'pt-BR': 'Índia',
-          ru: 'Индия',
-          'zh-CN': '印度',
+          de: 'USA',
+          en: 'United States',
+          es: 'Estados Unidos',
+          fr: 'États Unis',
+          ja: 'アメリカ',
+          'pt-BR': 'EUA',
+          ru: 'США',
+          'zh-CN': '美国',
         },
         isInEuropeanUnion: false,
       },
       traits: {
+        ipAddress: '8.8.8.8',
         isAnonymous: false,
         isAnonymousProxy: false,
         isAnonymousVpn: false,
@@ -82,45 +84,14 @@ describe('CityService', () => {
         isResidentialProxy: false,
         isSatelliteProvider: false,
         isTorExitNode: false,
-        ipAddress: '2401:4900:1c82:377e:bb89:dfc9:28a1:1e06',
-        network: '2401:4900:1c82::/47',
-      },
-      city: {
-        geonameId: 1264733,
-        names: {
-          de: 'Lucknow',
-          en: 'Lucknow',
-          es: 'Lucknow',
-          fr: 'Lucknow',
-          ja: 'ラクナウ',
-          'pt-BR': 'Lucknow',
-          ru: 'Лакхнау',
-          'zh-CN': '勒克瑙',
-        },
+        network: '8.8.8.0/23',
       },
       location: {
-        accuracyRadius: 200,
-        latitude: 26.8756,
-        longitude: 80.9115,
-        timeZone: 'Asia/Kolkata',
+        accuracyRadius: 1000,
+        latitude: 37.751,
+        longitude: -97.822,
+        timeZone: 'America/Chicago',
       },
-      postal: {
-        code: '226016',
-      },
-      subdivisions: [
-        {
-          geonameId: 1253626,
-          isoCode: 'UP',
-          names: {
-            en: 'Uttar Pradesh',
-            fr: 'Uttar Pradesh',
-            ja: 'ウッタル・プラデーシュ州',
-            'pt-BR': 'Utar Pradexe',
-            ru: 'Уттар-Прадеш',
-            'zh-CN': '北方邦',
-          },
-        },
-      ],
     };
     const result = service.getCity(ip);
     expect(JSON.parse(JSON.stringify(result))).toEqual(mockCityInfo);
