6615a7160ded6a94c0dacea982c1250a277adb03
35C4n0r <jaykumar20march@gmail.com>
2024-07-22 22:03:40 +0530

    chore: refactor models to modules
    Signed-off-by: 35C4n0r <jaykumar20march@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CityModule } from './models/city/city.module';
-import { GeorevModule } from './models/georev/georev.module';
-import { LocationModule } from './models/location/location.module';
+import { CityModule } from './modules/city/city.module';
+import { GeorevModule } from './modules/georev/georev.module';
+import { LocationModule } from './modules/location/location.module';
 import { ConfigModule } from '@nestjs/config';
-import { config } from './config/config';
 
 @Module({
   imports: [
@@ -14,11 +11,10 @@ import { config } from './config/config';
     LocationModule,
     ConfigModule.forRoot({
       envFilePath: `${process.cwd()}/config/env/${process.env.NODE_ENV || 'default'}.env`,
-      load: [config],
       isGlobal: true,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
